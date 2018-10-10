f5b2feaa05f28c7594b9437620d8a2db632d0411
Asens <asens1314@163.com>
2018-10-10 18:11:10 +0800

    init

--- sso_client/src/main/java/cn/asens/controller/TestAct.java
+++ sso_client/src/main/java/cn/asens/controller/TestAct.java
@@ -1,15 +1,24 @@
 package cn.asens.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @author Asens
  */
 @RestController
 public class TestAct {
+    @Resource
+    private RestTemplate restTemplate;
+
     @RequestMapping("/")
     public Object main() {
+        ResponseEntity<String> result=
+                restTemplate.getForEntity("http://sso_server/",String.class);
         return "index";
     }
 
